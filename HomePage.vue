3c28d5dfed9cc25eb5e2933c4acc67efe6c56668
Taylor <taylorhoo@yandex.com>
2020-12-01 18:27:32 +0000

    initial commit - import from CU repository

---
+++ HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png"  aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <router-link to="/build">Get started</router-link> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  created() {
+    console.log(process.env.VUE_APP_TEST, process.env.SOME_TEST);
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 25px;
+}
+</style>
